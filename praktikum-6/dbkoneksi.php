9a34afd39ec8fc19617d2814ced4ebc9b4bd8f0b
teggar4ar <114556888+teggar4ar@users.noreply.github.com>
2023-05-16 14:19:23 +0700

    Add files via upload

---
+++ praktikum-6/dbkoneksi.php
@@ -0,0 +1,20 @@
+<?php
+
+    $host = 'localhost';
+    $dbname = 'dbkoperasi';
+    $username = 'root';
+    $password = 
+    '';
+
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$dbname",$username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Koneksi Gagal: " . $e->getMessage();
+    }
+
+
+
+
+
+?>
